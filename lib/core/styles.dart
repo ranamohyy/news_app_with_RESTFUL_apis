3166d1ee4b5a8bef061d89636f44c2d6ec6c53ca
ranamohyy <ranaamr22@gmail.com>
2025-07-10 03:57:23 +0300

    make styles for text in app

---
+++ lib/core/styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppStyles {
+  static final textStyle15 = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  static final textStyle12Grey = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey,
+  );
+  static final textStyle30 = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+}
