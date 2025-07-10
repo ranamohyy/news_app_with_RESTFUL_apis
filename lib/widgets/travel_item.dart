fb7b8f6beebeb8090ce392010bdc7bfb46abb627
ranamohyy <ranaamr22@gmail.com>
2025-07-10 03:59:15 +0300

    make the item in store with its details

---
+++ lib/widgets/travel_item.dart
@@ -0,0 +1,44 @@
+part of 'package:news_app/home/view.dart';
+
+class TravelItemWidget extends StatelessWidget {
+  const TravelItemWidget({
+    super.key,
+    required this.title,
+    required this.date,
+    required this.image,
+  });
+  final String title;
+  final String date;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 15.w,
+      children: [
+        Expanded(
+            flex: 2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: AppStyles.textStyle15,
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Text(
+                  date,
+                  style: AppStyles.textStyle12Grey,
+                ),
+              ],
+            )),
+        Expanded(
+            child: Image.asset(
+          image,
+        ))
+      ],
+    );
+  }
+}
