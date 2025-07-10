c61d19fe83cf1ce3b456ba15b448e8655ad56ca6
ranamohyy <ranaamr22@gmail.com>
2025-07-10 04:00:14 +0300

    add assets

---
+++ lib/core/assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static String airPods = "assets/images/air_pods.png";
+  static String lamp = "assets/images/lamp.png";
+  static String nile = "assets/images/nile.png";
+  static String ballon = "assets/images/ballon.png";
+}
