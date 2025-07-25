7fb9e3672af37a6d43b978c05aef3e04a4298ed8
ranamohyy <ranaamr22@gmail.com>
2025-07-25 17:13:08 +0300

    make function for getting data

---
+++ lib/core/network_services/home_services.dart
@@ -0,0 +1,23 @@
+import 'dart:developer';
+import 'package:news_app/core/constans.dart';
+import 'package:news_app/core/network_services/dio_helper.dart';
+import 'package:news_app/core/network_services/end_points.dart';
+import 'package:news_app/models/news_home_model.dart';
+
+class HomeServices {
+  getHomeData() async {
+    try {
+      final response = await DioHelper.getData(EndPoints.homeEndPoint, query: {
+        'country': 'us',
+        'apiKey': AppConstans.apiKey,
+      });
+      if (response.statusCode == 200) {
+        NewsHomeModel newsHomeModel = NewsHomeModel.fromJson(response.data);
+        return newsHomeModel;
+      }
+    } catch (e) {
+      log(e.toString());
+      return Future.error(e.toString());
+    }
+  }
+}
