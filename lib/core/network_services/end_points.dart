ddd7cee8cf66d4647e201f63dc18b499b70f6301
ranamohyy <ranaamr22@gmail.com>
2025-07-27 02:58:45 +0300

    edit search end point

--- lib/core/network_services/end_points.dart
+++ lib/core/network_services/end_points.dart
@@ -1,4 +1,4 @@
 class EndPoints {
   static const String homeEndPoint = 'top-headlines';
-  static const String searchEndPoint = 'v2/everything';
+  static const String searchEndPoint = 'everything';
 }
