af0f484a2dcfd15836703bb347e21f5b40d3f09c
ranamohyy <ranaamr22@gmail.com>
2025-07-27 03:03:12 +0300

    add go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/navigator/app_nav.dart';
 import 'package:news_app/core/network_services/dio_helper.dart';
 import 'package:news_app/home/view.dart';
 import 'package:news_app/search/view.dart';
@@ -20,16 +21,16 @@ class MyApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => MaterialApp(
+      builder: (context, child) => MaterialApp.router(
+        routerConfig: RouterGenerator.goRoute,
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'News App',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: child,
       ),
-      child: SearchResultsScreen(),
+      // child: HomePageScreen(),
     );
   }
 }
