8ab8feb90f6d9bd84b41b08ac1a64b3cbecac4e0
ranamohyy <ranaamr22@gmail.com>
2025-07-31 18:09:42 +0300

    start from home

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/core/navigator/app_nav.dart';
 import 'package:news_app/core/network_services/dio_helper.dart';
-import 'package:news_app/home/view.dart';
-import 'package:news_app/search/view.dart';
-import 'package:news_app/search_results/view.dart';
 
 void main() {
   DioHelper.init();
@@ -30,7 +27,6 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
       ),
-      // child: HomePageScreen(),
     );
   }
 }
